168b144c02e296fc1a77f52fb6946d8e28caa380
Gabriel <eugabrielfa07@gmail.com>
2025-11-11 20:40:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Adicione esta linha:
-  base: '/tiamat/', 
+  base: '/teste/', 
 
   plugins: [react()],
   optimizeDeps: {
